9e97548f86889e24bfb95b41e12cb89a09380f4a
scriptype <scriptyper@gmail.com>
2023-01-14 07:07:23 +0200

    Introduce CLI

---
+++ cli/manual.js
@@ -0,0 +1,16 @@
+module.exports = `
+  Writ
+  ====
+
+  Interface:
+  $ writ [start|build] [dir=.]
+
+  Start local development:
+  $ writ start .
+
+  Produce a build output:
+  $ writ build .
+
+  Use a different source directory:
+  $ writ <start|build> ../the-other-directory
+`
