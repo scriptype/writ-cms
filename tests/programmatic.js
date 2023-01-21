2053ebd8b4c5ec3b18df08b3a44f6baf86cfa7f1
scriptype <scriptyper@gmail.com>
2023-01-21 20:59:35 +0200

    Remove img alt and title for now
    An untouched caption property is needed for these usages

--- tests/programmatic.js
+++ tests/programmatic.js
@@ -1,6 +1,6 @@
 const writ = require('../')
 
-writ.build({
-    rootDirectory: '/Users/enes/code/writ',
-    debug: true
-  })
+writ.start({
+  rootDirectory: '/Users/enes/code/writ',
+  debug: true
+})
