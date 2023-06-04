4c1ede6b7414f4880d11f887835dde7d2215d98f
scriptype <scriptyper@gmail.com>
2023-06-05 02:27:45 +0300

    Create categories.json in debug mode

--- src/compiler/rendering/views/posts-json.js
+++ src/compiler/rendering/views/posts-json.js
@@ -1,16 +1,27 @@
 const { join } = require('path')
 const { writeFile } = require('fs/promises')
 const Settings = require('../../../settings')
-const { debugLog } = require('../../../debug')
+const { getDebug, debugLog } = require('../../../debug')
 
-const renderPostsJSON = ({ postsJSON }) => {
+const renderPostsJSON = ({ postsJSON, categories }) => {
   const { out } = Settings.getSettings()
   const outPath = join(out, 'posts.json')
   debugLog('creating:', outPath)
-  return writeFile(
-    outPath,
-    JSON.stringify(postsJSON, null, 2)
-  )
+  const writes = [
+    writeFile(
+      outPath,
+      JSON.stringify(postsJSON, null, 2)
+    )
+  ]
+  if (getDebug()) {
+    const outPath2 = join(out, 'categories.json')
+    debugLog('creating:', outPath2)
+    writes.push(writeFile(
+      outPath2,
+      JSON.stringify(categories, null, 2)
+    ))
+  }
+  return Promise.all(writes)
 }
 
 module.exports = renderPostsJSON
