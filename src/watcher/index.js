9be5b3dcdfe05ac5015c411256dff36a4539bd61
scriptype <scriptyper@gmail.com>
2023-10-11 17:08:41 +0300

    Fix watcher infinite re-run

--- src/watcher/index.js
+++ src/watcher/index.js
@@ -1,42 +1,49 @@
-const bs = require('browser-sync').create()
 const { resolve } = require('path')
+const _ = require('lodash')
+const bs = require('browser-sync').create()
 const Settings = require('../settings')
 const Debug = require('../debug')
 const api = require('./api')
-
-const watchOptions = {
-  reloadDebounce: 500,
-  ignoreInitial: true,
-  ignored: new RegExp(
-    [
-      Settings.getSettings().exportDirectory,
-      'node_modules',
-      '.git',
-      '.DS_Store',
-      'package.json',
-      'package-lock.json'
-    ].join('|')
-  )
-}
-
-Debug.debugLog('watch options', watchOptions)
 
 module.exports = {
   init({ decorators, onChange }) {
     const { rootDirectory, exportDirectory } = Settings.getSettings()
     const watchDir = resolve(rootDirectory)
     const serverDir = resolve(rootDirectory, exportDirectory)
+    const watchOptions = {
+      reloadDebounce: 100,
+      ignoreInitial: true,
+      ignored: new RegExp(
+        [
+          exportDirectory,
+          'node_modules',
+          '.git',
+          '.DS_Store',
+          'package.json',
+          'package-lock.json'
+        ].join('|')
+      )
+    }
 
+    Debug.debugLog('watch options', watchOptions)
     Debug.debugLog('watch', watchDir)
     Debug.debugLog('serve', serverDir)
 
     let compilePromise = Promise.resolve()
 
-    bs.watch(watchDir, watchOptions, (e, file) => {
+    const cb = _.debounce((e, file) => {
       console.log(new Date(), file)
-      compilePromise = compilePromise.then(onChange)
-      bs.reload()
-    })
+      compilePromise = compilePromise
+        .then(onChange)
+        .then(() => {
+          if (cb.cancel) {
+            cb.cancel()
+          }
+          return Promise.resolve()
+        })
+    }, 2000, { leading: true })
+
+    bs.watch(watchDir, watchOptions, cb)
 
     bs.init({
       server: serverDir,
