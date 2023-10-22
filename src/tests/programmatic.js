33abd322ac7d6e8ab9a48d1a8b56aaa1cb410edb
scriptype <scriptyper@gmail.com>
2023-10-22 23:11:57 +0300

    Add single txt file test that fails due to VersionControl

--- src/tests/programmatic.js
+++ src/tests/programmatic.js
@@ -1,6 +1,6 @@
-const { rm, mkdir, readdir } = require('fs/promises')
+const { rm, mkdir, readdir, writeFile } = require('fs/promises')
 const { join, resolve } = require('path')
-const test = require('ava')
+const test = require('tape')
 const writ = require('..')
 
 const randomNumber = () => {
@@ -12,6 +12,7 @@ const createTempDir = async () => {
   await mkdir(dirName)
   return {
     name: dirName,
+    mkFile: (name, content) => writeFile(join(dirName, name), content),
     rm: () => rm(dirName, { recursive: true })
   }
 }
@@ -32,6 +33,49 @@ test('builds in empty directory', async t => {
   t.true(
     exportDirectoryContents.includes('index.html'),
     'Export directory has index.html'
+  )
+  t.true(
+    exportDirectoryContents.includes(assetsDirectory),
+    'Export directory has assets directory'
+  )
+
+  const assetsDirectoryContents = await readdir(join(dir.name, exportDirectory, assetsDirectory))
+  t.true(
+    assetsDirectoryContents.includes('common'),
+    'Assets directory has common assets'
+  )
+  t.true(
+    assetsDirectoryContents.includes('default'),
+    'Assets directory has theme-default assets'
+  )
+})
+
+test('builds with a single txt file', async t => {
+  const dir = await createTempDir()
+  t.teardown(dir.rm)
+  const fileNameIn = 'hello.txt'
+  const fileNameOut = 'hello.html'
+  dir.mkFile(fileNameIn, 'Hello!')
+
+  const { exportDirectory, assetsDirectory } = writ.getDefaultSettings()
+  await writ.build({
+    rootDirectory: dir.name
+  })
+
+  const dirContents = await readdir(dir.name)
+  t.true(
+    dirContents.includes(fileNameIn),
+    `${fileNameIn} exists`
+  )
+  t.true(
+    dirContents.includes(exportDirectory),
+    'Creates exportDirectory'
+  )
+
+  const exportDirectoryContents = await readdir(join(dir.name, exportDirectory))
+  t.true(
+    exportDirectoryContents.includes(fileNameOut),
+    `Export directory has compiled ${fileNameOut}`
   )
   t.true(
     exportDirectoryContents.includes(assetsDirectory),
