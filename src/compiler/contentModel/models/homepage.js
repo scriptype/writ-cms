0a229c1ae28ea0fcaedc66719d1e57f79f6a7f11
scriptype <scriptyper@gmail.com>
2024-07-04 17:42:44 +0300

    If homepage has .html extension, render as it is
    Instead of using basic homepage template

--- src/compiler/contentModel/models/homepage.js
+++ src/compiler/contentModel/models/homepage.js
@@ -2,18 +2,20 @@ const Settings = require('../../../settings')
 const contentTypes = require('../contentTypes')
 const parseTemplate = require('../parseTemplate')
 
-const createHomepage = (fsObject) => {
+const createHomepage = (fsObject = {}) => {
   const permalink = Settings.getSettings().permalinkPrefix
 
   const metadata = fsObject ?
     parseTemplate(fsObject, { permalink }) :
     { attributes: {} }
 
+  const type = fsObject.extension === '.html' ? 'raw-index-html' : 'basic'
+
   return {
     ...(fsObject || {}),
     type: contentTypes.HOMEPAGE,
     data: {
-      type: metadata.type || 'basic',
+      type: metadata.type || type,
       title: metadata.title || '',
       content: metadata.content || '',
       mentions: metadata.mentions || [],
