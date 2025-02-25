c7ee3629b7fcb68f3d19e23d592b1560d15d6719
scriptype <scriptyper@gmail.com>
2025-02-25 12:05:46 +0300

    Try fixing windows path sep issue

--- src/tests/compiler/contentModel2.js
+++ src/tests/compiler/contentModel2.js
@@ -1,7 +1,10 @@
+const os = require('os')
 const test = require('tape')
 const contentModel2 = require('../../compiler/contentModel2')
-const FSTree = require('../fixtures/contentModel2FSTree.json')
 
+const FSTree = os.platform() === 'win32' ?
+  require('../fixtures/contentModel2FSTreeWin32.json') :
+  require('../fixtures/contentModel2FSTree.json')
 
 test('compiler/contentModel2', t => {
   t.test('exits 0', async () => {
