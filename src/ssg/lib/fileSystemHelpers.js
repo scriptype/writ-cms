1a5c9ca094066d141b2484516a4e2d52fc6e63b4
scriptype <scriptyper@gmail.com>
2026-01-12 03:04:28 +0300

    #19 | Sleep 100ms in atomicReplace
    Making it bigger slows tests down. Nuking this atomic business is
    imminent

--- src/ssg/lib/fileSystemHelpers.js
+++ src/ssg/lib/fileSystemHelpers.js
@@ -40,7 +40,7 @@ const ensureDirectory = async (path) => {
  * Building in a separate temp location releases all file handles
  * before the final swap.
  */
-const atomicReplace = async (targetPath, buildFn, sleepTime = 1000) => {
+const atomicReplace = async (targetPath, buildFn, sleepTime = 100) => {
   const sleep = (duration) => new Promise(resolve => setTimeout(resolve, duration))
   const tempPath = await atomicFS.mkdtemp(join(tmpdir(), 'atomic-replace-'))
   try {
