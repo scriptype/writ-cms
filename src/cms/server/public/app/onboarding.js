e47ccfd6b30f0382dfebaadc1ab1513130873c14
scriptype <scriptyper@gmail.com>
2026-02-25 22:46:35 +0300

    Start with a random project name instd of prompting

--- src/cms/server/public/app/onboarding.js
+++ src/cms/server/public/app/onboarding.js
@@ -1,10 +1,30 @@
 import api from '../api.js'
 import editProject from './editProject.js'
 
+const randomFrom = (list) => list[Math.floor(Math.random() * list.length)]
+
+const randomProjectNames = [
+  'The World',
+  'My New Thing',
+  'La Isla Bonita',
+  'Ceci n\'est pas une pipe',
+  'Aquaris',
+  'Sunshine Recorder',
+  'Aurora Borealis',
+  'A Great Oak',
+  'Brave New World',
+  'A Brief History of Everything',
+  'The year my parents went on vacation',
+  'Something new',
+  'Lorem site',
+  'Bits and bops',
+  'VGA vs HDMI'
+]
+
 const onboarding = async () => {
   console.log('first time onboarding')
   const newProject = await api.workspace.createProject({
-    name: prompt('Project name')
+    name: randomFrom(randomProjectNames)
   })
   return editProject({
     ssgOptions: {
